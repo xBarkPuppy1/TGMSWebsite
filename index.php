6714bcd789463fee9ccad77fdb97a7f47ffabb59
Barkpuppy1 the overlord of masterfreedom <bbarked@gmail.com>
2017-02-02 16:05:21 -0600

    Fixed

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>xBarkPuppy1 Website index</title>
+        <title>TGMS Website index</title>
         <link href="style.css" media="all" rel="stylesheet" type="text/css" />
         <link href="markdown.css" media="all" rel="stylesheet" type="text/css" />
     </head>
